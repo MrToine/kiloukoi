bed8a78e6dc2a059461cf9423e7a7a57d058b70a
Anhony <anthony.violet@outlook.be>
2024-02-25 12:08:25 +0100

    correction autlogin 2

--- database/migrations/2024_02_25_104417_modify_remeber_token_in_table_users.php
+++ database/migrations/2024_02_25_104417_modify_remeber_token_in_table_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('remember_token')->change();
+            $table->text('remember_token')->nullable()->change();
         });
     }
 
